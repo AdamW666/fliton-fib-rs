7ccc6a327950b092020135cd60a7d1e012f3f0e2
Adam Wong <wizzardcloud@gmail.com>
2022-05-16 11:42:54 +1000

    Add creating Python class in rust code

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
+mod class_module;
 mod fib_calcs;
 mod interface;
 
@@ -10,6 +11,8 @@ use fib_calcs::fib_numbers::__pyo3_get_function_fibonacci_numbers;
 use interface::config::__pyo3_get_function_run_config;
 
 use interface::object::__pyo3_get_function_object_interface;
+
+use class_module::fib_processor::FibProcessor;
 
 #[pyfunction]
 fn say_hello() {
@@ -23,6 +26,7 @@ fn fliton_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(fibonacci_numbers)).unwrap();
     m.add_wrapped(wrap_pyfunction!(run_config)).unwrap();
     m.add_wrapped(wrap_pyfunction!(object_interface)).unwrap();
+    m.add_class::<FibProcessor>()?;
 
     Ok(())
 }
